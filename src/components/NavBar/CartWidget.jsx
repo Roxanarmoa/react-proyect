7c14d84c1d0b1d56ea9a61d87d937ddb4d57fc20
Roxana Armoa <roxanarmoa@gmail.com>
2023-12-06 21:33:49 -0300

    arreglando errores, agregando sass

--- src/components/NavBar/CartWidget.jsx
+++ src/components/NavBar/CartWidget.jsx
@@ -1,7 +1,9 @@
-import "./NavBar.css"
+import "./NavBar.scss"
+
 const CartWidget = () => {
     return (
         <div id="cartwidget">
+            <p>1</p>
             <img src="/img/cart.png" alt="" />
         </div>
     )
